33814a4edf9cc8ec917b1c517475abc6619edf56
sh3ngsh3ng <yshenglim13@gmail.com>
2022-04-04 07:35:16 +0000

    s

---
+++ test.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+function disp(person) {
+    console.log("".concat(person.name, " ").concat(person.country));
+}
+var p = {
+    name: "Yong",
+    country: "Singapore"
+};
+disp(p);
--- test.ts
+++ test.ts
@@ -1,1 +1,29 @@
-// typescript
+// Typescript
+// tsc.cmd <file-name> --watch
+export{}
+
+interface Person {
+    name : string;
+    country : string;
+}
+
+function disp(person: Person) {
+    console.log(`${person.name} ${person.country}`)
+}
+
+let p = {
+    name: "Yong",
+    country: "Singapore"
+}
+
+disp(p)
+
+
+
+
+
+
+
+
+
+
