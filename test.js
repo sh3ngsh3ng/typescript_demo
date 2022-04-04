60895a573b0f69ce83388b637ae1168025108fcb
sh3ngsh3ng <yshenglim13@gmail.com>
2022-04-04 07:53:57 +0000

    s

--- test.js
+++ test.js
@@ -1,10 +1,28 @@
 "use strict";
 exports.__esModule = true;
-function disp(person) {
-    console.log("".concat(person.name, " ").concat(person.country));
-}
-var p = {
-    name: "Yong",
-    country: "Singapore"
-};
-disp(p);
+// Abstraction
+// interface Person {
+//     name : string;
+//     country : string;
+// }
+// function disp(person: Person) {
+//     console.log(`${person.name} ${person.country}`)
+// }
+// let p = {
+//     name: "Yong",
+//     country: "Singapore"
+// }
+// disp(p)
+// Inheritance
+var Trainee = /** @class */ (function () {
+    function Trainee(name) {
+        this.traineeName = name;
+    }
+    Trainee.prototype.display = function () {
+        console.log(this.traineeName);
+    };
+    return Trainee;
+}());
+var t = new Trainee("Yong");
+console.log(t.traineeName);
+t.display();
--- test.ts
+++ test.ts
@@ -3,21 +3,45 @@
 export{}
 
 // Abstraction
-interface Person {
-    name : string;
-    country : string;
+// interface Person {
+//     name : string;
+//     country : string;
+// }
+
+// function disp(person: Person) {
+//     console.log(`${person.name} ${person.country}`)
+// }
+
+// let p = {
+//     name: "Yong",
+//     country: "Singapore"
+// }
+
+// disp(p)
+
+// Inheritance
+class Trainee {
+    traineeName: string;
+    constructor(name: string) {
+        this.traineeName = name;
+    }
+
+    display() {
+        console.log(this.traineeName);
+    }
 }
 
-function disp(person: Person) {
-    console.log(`${person.name} ${person.country}`)
-}
 
-let p = {
-    name: "Yong",
-    country: "Singapore"
-}
+let t = new Trainee("Yong");
+console.log(t.traineeName);
+t.display();
 
-disp(p)
+
+
+
+
+
+
 
 
 
